7ec5f66239cb0589eab197ec8b597a15a787e265
Julian Popescu <jpopesculian@gmail.com>
2015-09-05 14:53:09 -0400

    pylinter

--- api/controllers/Root.py
+++ api/controllers/Root.py
@@ -7,7 +7,3 @@ class RootController(Controller):
     @route('/')
     def index(self):
         return self.send_static_file('index.html')
-
-    @route('/test')
-    def test(self):
-        return {'test': 'hello'}
